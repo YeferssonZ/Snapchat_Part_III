03fc4f2861d85aa34b4a29eda6dc1890b280d342
YeferssonZ <yefersson.zuniga@tecsup.edu.pe>
2023-11-11 15:51:20 -0800

    Laboratorio 11 PMA completado (inicio de sesión con Google y Teléfono)!!!

--- ZunigaSnapchat/iniciarSesionViewController.swift
+++ ZunigaSnapchat/iniciarSesionViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseCore
+import GoogleSignIn
 
 class iniciarSesionViewController: UIViewController {
 
@@ -21,15 +23,15 @@ class iniciarSesionViewController: UIViewController {
         // Configuración adicional si es necesaria
     }
 
+    // MARK: - Autenticación con Correo y Contraseña
     @IBAction func iniciarSesionTapped(_ sender: Any) {
-        // Autenticación con correo y contraseña
         Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
             self.handleAuthResult(user, error)
         }
     }
 
+    // MARK: - Autenticación con Teléfono
     @IBAction func iniciarSesionTelefonoTapped(_ sender: Any) {
-        // Autenticación con teléfono
         guard let phoneNumber = phoneNumberTextField.text else { return }
 
         PhoneAuthProvider.provider().verifyPhoneNumber(phoneNumber, uiDelegate: nil) { (verificationID, error) in
@@ -46,7 +48,7 @@ class iniciarSesionViewController: UIViewController {
         }
     }
 
-    // Función para manejar el resultado de la autenticación
+    // MARK: - Manejo del Resultado de Autenticación
     private func handleAuthResult(_ user: AuthDataResult?, _ error: Error?) {
         print("Intentando Iniciar Sesión")
         if let error = error {
@@ -57,7 +59,7 @@ class iniciarSesionViewController: UIViewController {
         }
     }
 
-    // Función para mostrar un UIAlertController para ingresar el código de verificación
+    // MARK: - Mostrar Alerta para Código de Verificación
     private func mostrarAlertaParaCodigo() {
         let alertController = UIAlertController(title: "Código de Verificación", message: "Ingrese el código enviado por mensaje de texto", preferredStyle: .alert)
 
@@ -76,7 +78,7 @@ class iniciarSesionViewController: UIViewController {
         present(alertController, animated: true, completion: nil)
     }
 
-    // Función para verificar el código de verificación del teléfono
+    // MARK: - Verificar Código de Verificación del Teléfono
     func verificarCodigoDeVerificacion(_ codigo: String) {
         guard let verificationID = verificationID else { return }
 
@@ -84,6 +86,39 @@ class iniciarSesionViewController: UIViewController {
 
         Auth.auth().signIn(with: credential) { (user, error) in
             self.handleAuthResult(user, error)
+        }
+    }
+
+    // MARK: - Autenticación con Google
+    @IBAction func iniciarSesionGoogleTapped(_ sender: Any) {
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+
+        let config = GIDConfiguration(clientID: clientID)
+        GIDSignIn.sharedInstance.configuration = config
+
+        GIDSignIn.sharedInstance.signIn(withPresenting: self) { [unowned self] result, error in
+            guard error == nil else {
+                print("Error al iniciar sesión con Google: \(error!.localizedDescription)")
+                return
+            }
+
+            guard let user = result?.user,
+                  let idToken = user.idToken?.tokenString
+            else {
+                print("Error al obtener información del usuario de Google")
+                return
+            }
+
+            let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: user.accessToken.tokenString)
+
+            Auth.auth().signIn(with: credential) { result, error in
+                if let error = error {
+                    print("Error al iniciar sesión con Google: \(error.localizedDescription)")
+                } else {
+                    print("Inicio de sesión exitoso con Google")
+                    // Puedes realizar acciones adicionales aquí después de un inicio de sesión exitoso
+                }
+            }
         }
     }
 }
